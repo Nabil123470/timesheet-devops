06912fb1b00da7be24a92727ce25d478216c125c
Nabil123470 <nabil.daghbari39@gmail.com>
2021-11-02 22:16:55 +0100

    changer la classe EntrepriseServiceImpl.java

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -16,7 +16,7 @@ import tn.esprit.spring.repository.EntrepriseRepository;
 
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
-
+ // test Junit 
 	@Autowired
     EntrepriseRepository entrepriseRepoistory;
 	@Autowired
